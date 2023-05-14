f4ae4f3dcc150d8efb46968ba611c147a35cdb04
Michele-ati <110529983+Michele-ati@users.noreply.github.com>
2023-05-14 20:02:09 +0200

    import model corretto

--- print_product_labels/__manifest__.py
+++ print_product_labels/__manifest__.py
@@ -1,36 +1,29 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "print_product_labels",
+    'name': "print_product_labels 1",
 
     'summary': """
-       prodotto""",
-
-    'description': """
-        Long description of module's purpose
+        Print product labels
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Print product labels
+    """,
+
+    'author': "Huroos",
+    'website': "https://www.huroos.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Product',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product'],
-
-    # always loaded
-    # NB i report vanno inseriti in fondo alla lista dei data
+    'images': ['static/description/icon.png'],
     'data': [
-        # 'security/ir.model.access.csv',
         'data/report_paperformat.xml',
-        'report/print_label_product_report.xml',
-        'report/print_etichetta.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'report/product_product_templates.xml',
+        'report/product_reports.xml',
     ],
 }
--- print_product_labels/models/product_product.py
+++ print_product_labels/models/product_product.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class ProductProduct(models.Model):
@@ -8,8 +6,5 @@ class ProductProduct(models.Model):
     def get_price(self):
         pricelist_id = self.env['product.pricelist'].search([], limit=1)
         price = pricelist_id.get_product_price(self, 1, False)
-        price_tax_included = self.taxes_id.compute_all(price, handle_price_include=False)['total_included']
-        return '€ ' + str('%.2f' % round(price_tax_included, 2)).replace('.', ',')
-
-
-
+        #price_tax_included = self.taxes_id.compute_all(price, handle_price_include=False)['total_included']
+        return '€ ' + str('%.2f' % round(price, 2)).replace('.', ',')
